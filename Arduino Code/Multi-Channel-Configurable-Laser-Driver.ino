341ff18e5a0af4c12b73f9157bc4b4b528e3533d
El-Rayes <kelray@users.noreply.github.com>
2026-06-06 16:24:08 -0700

    --

---
+++ Arduino Code/Multi-Channel-Configurable-Laser-Driver.ino
@@ -0,0 +1,60 @@
+// Basic example driving Channel 1 and 2 of the multi-channel Configurable LED/Laser Driver
+// The code requires the following libraries: MCP48xx and uTimerLib
+
+#include "Arduino.h"
+#include "uTimerLib.h"
+#include <MCP48xx.h>
+
+#ifndef LED_BUILTIN
+	// change to fit your needs
+	// Use 0 or 1 to use DigiSpark AVR internal LED (depending revision, mine is 1)
+	#define LED_BUILTIN 13
+#endif
+
+uint16_t SineLUT64[] = { 2048, 2248, 2447, 2642, 2831, 3013, 3185, 3346,
+3495, 3630, 3750, 3853, 3939, 4007, 4056, 4085,
+4095, 4085, 4056, 4007, 3939, 3853, 3750, 3630,
+3495, 3346, 3185, 3013, 2831, 2642, 2447, 2248,
+2048, 1847, 1648, 1453, 1264, 1082, 910, 749,
+600, 465, 345, 242, 156, 88, 39, 10,
+0, 10, 39, 88, 156, 242, 345, 465,
+600, 749, 910, 1082, 1264, 1453, 1648, 1847};
+
+MCP4822 dac(5);
+uint8_t counter = 0;
+
+volatile bool status = 0;
+
+void timed_function() 
+{
+  status = !status;
+  dac.setVoltageA(SineLUT64[counter]);
+  dac.updateDAC();
+  if(counter < 63)
+       counter++;
+else
+    counter = 0;
+}
+
+void setup() 
+{
+  // We call the init() method to initialize the instance
+  dac.init();
+
+  // The channels are turned off at startup so we need to turn the channel we need on
+  dac.turnOnChannelA();
+  dac.turnOnChannelB();
+
+  // We configure the channels in High gain
+  // It is also the default value so it is not really needed
+  dac.setGainA(MCP4822::High);
+  dac.setGainB(MCP4822::High);
+
+	pinMode(LED_BUILTIN, OUTPUT);
+	TimerLib.setInterval_us(timed_function, 50);
+}
+
+void loop() 
+{
+	digitalWrite(LED_BUILTIN, status);
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Multi-Channel-Configurable-Laser-Driver
-A low-cost, Arduino-friendly four channel programmable LED/Laser driver
+A low-cost, Arduino-friendly four channel programmable LED/Laser driver using  750mA sink LED driver from Diode Inc. The current is controlled using ESP32-based NodeMCU32S microcontroller board. 
+This driver is designed to be low-cost built on a 2-layers PCB, inexpensive microcontroller platform and LED drivers IC "AL5871" to make it affordable to hobbyists, students and low-budget projects.
